4f9dab91b8a046bd63a8fb00a3fde11420fb2633
Reynaldo <salcedoreynaldo5@gmail.com>
2022-11-02 20:28:37 -0400

    Added horizontal movement

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -5,8 +5,8 @@ func _physics_process(delta):
 
 func player_move_input():
 	if Input.is_action_pressed("move_left"):
-		motion.x = clamp(motion.x-speed, -speed, 0)
+		motion.x = clamp(motion.x - speed, -max_speed, 0)
 	elif Input.is_action_pressed("move_right"):
-		motion.x = speed
+		motion.x = clamp(motion.x + speed, 0, max_speed)
 	else:
-		motion.x = 0
+		motion.x = lerp(motion.x, 0, friction)
