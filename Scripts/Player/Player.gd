190f1b9379e8ee47715f1810832272aed126557e
Reynaldo <salcedoreynaldo5@gmail.com>
2022-11-02 20:25:16 -0400

    Started adding movement

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -2,3 +2,11 @@ extends Character
 
 func _physics_process(delta):
 	move_and_slide(motion)
+
+func player_move_input():
+	if Input.is_action_pressed("move_left"):
+		motion.x = clamp(motion.x-speed, -speed, 0)
+	elif Input.is_action_pressed("move_right"):
+		motion.x = speed
+	else:
+		motion.x = 0
